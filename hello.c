74647fe9365c697956cc99ff76f70dfc52f4abcd
EricmBar <111918496+EricmBar@users.noreply.github.com>
2022-08-25 21:05:30 +0000

    added line after name

--- hello.c
+++ hello.c
@@ -11,7 +11,7 @@
 int main(int argc, char **argv) {
 
     printf("Hello World!\n");
-    printf("Eric Barber!!");
+    printf("Eric Barber!!\n");
     printf("Electrical Engineering\n");
 
     return 0;
