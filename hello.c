e907d58644538b0a73f8f54a5482eaece140f59e
EricmBar <111918496+EricmBar@users.noreply.github.com>
2022-08-25 20:47:32 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Eric Barber
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World!\n");
+    printf("Eric Barber!!");
+    printf("Electrical Engineering\n");
+
+    return 0;
+}
